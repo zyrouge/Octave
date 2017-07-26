841892d71db7a981614d7dd266837d51cfd0d82a
Avarel <avarelpm@gmail.com>
2017-07-26 16:25:52 -0500

    Shards revive update

--- src/main/kotlin/xyz/gnarbot/gnar/commands/executors/admin/RestartShardsCommand.kt
+++ src/main/kotlin/xyz/gnarbot/gnar/commands/executors/admin/RestartShardsCommand.kt
@@ -1,6 +1,8 @@
 package xyz.gnarbot.gnar.commands.executors.admin
 
+import net.dv8tion.jda.core.JDA
 import xyz.gnarbot.gnar.Bot
+import xyz.gnarbot.gnar.Shard
 import xyz.gnarbot.gnar.commands.Category
 import xyz.gnarbot.gnar.commands.Command
 import xyz.gnarbot.gnar.commands.CommandExecutor
@@ -8,26 +10,56 @@ import xyz.gnarbot.gnar.utils.Context
 
 @Command(
         id = 37,
-        aliases = arrayOf("restartShards"),
-        description = "Restart all Shard instances.",
+        aliases = arrayOf("revive", "restartShards"),
+        description = "Restart all shard instances.",
         admin = true,
         category = Category.NONE
 )
 class RestartShardsCommand : CommandExecutor() {
     override fun execute(context: Context, label: String, args: Array<String>) {
         if (args.isEmpty()) {
-            context.send().embed("Restarting Shards") {
-                desc { "Bot is now restarting." }
-            }.action().queue()
+            context.send().error("Try `all, dead, (#)`").queue()
+            return
+        }
 
-            Bot.restart()
-        } else {
-            val id = args[0].toIntOrNull()?.coerceIn(0, Bot.getShards().size) ?: kotlin.run {
-                context.send().error("You must enter a valid shard id.").queue()
-                return
+        when(args[0]) {
+            "all" -> {
+                context.send().embed("Restarting Shards") {
+                    desc { "Bot is now restarting." }
+                }.action().queue()
+
+                Bot.restart()
             }
+            "dead" -> {
+                val deadShards = Bot.getShards().filter { it.jda.status != JDA.Status.CONNECTED }
 
-            Bot.getShard(id).revive()
+                if (deadShards.isEmpty()) {
+                    context.send().info("Every shard is connected.").queue()
+                    return
+                }
+
+                context.send().embed("Restarting Shards") {
+                    desc {
+                        "Shards `${deadShards.map(Shard::id)}` are now restarting."
+                    }
+                }.action().queue()
+
+                deadShards.forEach(Shard::revive)
+            }
+            else -> {
+                val id = args[0].toIntOrNull()?.coerceIn(0, Bot.getShards().size) ?: kotlin.run {
+                    context.send().error("You must enter a valid shard id.").queue()
+                    return
+                }
+
+                context.send().embed("Restarting Shards") {
+                    desc {
+                        "Shards `$id` are now restarting."
+                    }
+                }.action().queue()
+
+                Bot.getShard(id).revive()
+            }
         }
     }
 }
