11694653313b8ca999fb21e4af2d67d8b5215426
MaxGrosshandler <maxgrosshandler@gmail.com>
2018-09-26 18:17:22 -0500

    create testing command
    want to see if I can make mod commands without actually cloning anything

---
+++ src/main/java/xyz/gnarbot/gnar/commands/executors/test/FakeCommand.java
@@ -0,0 +1,24 @@
+package xyz.gnarbot.gnar.commands.executors.general;
+
+import xyz.gnarbot.gnar.commands.BotInfo;
+import xyz.gnarbot.gnar.commands.Command;
+import xyz.gnarbot.gnar.commands.CommandExecutor;
+import xyz.gnarbot.gnar.commands.Context;
+
+@Command(
+        aliases = "fake",
+        description = "you should hide this command"
+)
+@BotInfo(
+        id = 33
+)
+public class FakeCommand extends CommandExecutor {
+    @Override
+    public void execute(Context context, String label, String[] args) {
+
+        context.getTextChannel().sendTyping().queue(ignored -> context.send().embed()
+                .field("why", true, 'you shouldt be seeing this')
+                .field("this is so bleeding", true, "i want to stop')
+                .action().queue());
+    }
+}
