08df1716ffcf025a572f5d5082fda6b5459e7d13
MaxGrosshandler <maxgrosshandler@gmail.com>
2018-09-27 21:11:57 -0500

    Update KickCommand.java

--- src/main/java/xyz/gnarbot/gnar/commands/executors/test/KickCommand.java
+++ src/main/java/xyz/gnarbot/gnar/commands/executors/test/KickCommand.java
@@ -1,1 +1,23 @@
-guild.getController().kick(user, reason).queue();
+
+package xyz.gnarbot.gnar.commands.executors.general;
+
+import xyz.gnarbot.gnar.commands.BotInfo;
+import xyz.gnarbot.gnar.commands.Command;
+import xyz.gnarbot.gnar.commands.CommandExecutor;
+import xyz.gnarbot.gnar.commands.Context;
+
+@Command(
+        aliases = "kick",
+        description = "i suck at everything"
+)
+@BotInfo(
+        id = 66686868
+)
+public class FakeCommand extends CommandExecutor {
+    @Override
+    public void execute(Context context, String label, String[] args) {
+
+        context.guild.getController().kick(args[0], args[1]).queue();
+    }
+}
+
