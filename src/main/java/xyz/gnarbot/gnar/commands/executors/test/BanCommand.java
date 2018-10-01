a4a162ab232d998191b1640b7d7f0dcc8f7c8284
MaxGrosshandler <maxgrosshandler@gmail.com>
2018-09-30 23:16:06 -0500

    mocked up a ban command

---
+++ src/main/java/xyz/gnarbot/gnar/commands/executors/test/BanCommand.java
@@ -0,0 +1,21 @@
+package xyz.gnarbot.gnar.commands.executors.general;
+
+import xyz.gnarbot.gnar.commands.BotInfo;
+import xyz.gnarbot.gnar.commands.Command;
+import xyz.gnarbot.gnar.commands.CommandExecutor;
+import xyz.gnarbot.gnar.commands.Context;
+
+@Command(
+        aliases = "ban",
+        description = "i can ban a deer"
+)
+@BotInfo(
+        id = 909320923
+)
+public class BanCommand extends CommandExecutor {
+    @Override
+    public void execute(Context context, String label, String[] args) {
+
+        context.guild.getController().ban(args[0], 7, args[1]).queue();
+    }
+}
