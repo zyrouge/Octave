558bc9392ce84224cf98f74ca80cb8562cc20d1a
kodehawa <david.alejandro.rubio@gmail.com>
2020-02-22 22:24:00 -0300

    Add the _vote command

---
+++ src/main/kotlin/xyz/gnarbot/gnar/commands/general/VoteCommand.kt
@@ -0,0 +1,26 @@
+package xyz.gnarbot.gnar.commands.general
+
+import xyz.gnarbot.gnar.commands.BotInfo
+import xyz.gnarbot.gnar.commands.Command
+import xyz.gnarbot.gnar.commands.CommandExecutor
+import xyz.gnarbot.gnar.commands.Context
+
+@Command(
+        aliases = ["vote"],
+        description = "Shows how to vote for the bot."
+)
+@BotInfo(
+        id = 50
+)
+class VoteCommand : CommandExecutor() {
+    override fun execute(context: Context, label: String, args: Array<String>) {
+        context.send().embed("Vote") {
+            desc {
+                buildString {
+                    append("Vote here to increase the visibility of the bot!\nIf you vote for Octave, you can get a normie box in Dank Memer everytime you vote too!\n")
+                    append("**[Vote by clicking here](https://discordbots.org/bot/gnar/vote)**\n")
+                }
+            }
+        }.action().queue()
+    }
+}
