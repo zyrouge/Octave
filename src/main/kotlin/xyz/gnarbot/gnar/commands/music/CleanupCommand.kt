1776d56ef337866c62af5f6f32601ed43aed0f04
Xevryll <maeyrlotg@gmail.com>
2020-03-08 23:49:48 -0500

    Slight optimization

--- src/main/kotlin/xyz/gnarbot/gnar/commands/music/CleanupCommand.kt
+++ src/main/kotlin/xyz/gnarbot/gnar/commands/music/CleanupCommand.kt
@@ -31,25 +31,21 @@ class CleanupCommand : CommandExecutor() {
         val purgeUser = if (context.message.mentionedUsers.isNotEmpty()) context.message.mentionedUsers[0].idLong else args[0]
 
         val queue = manager.scheduler.queue
-        var total = 0
         val removeSongs = ArrayList<AudioTrack>() //Prevent Concurrent Modification Exception
 
         for (song in queue) {
             if(purgeUser != "left") {
                 if (song.getUserData(TrackContext::class.java)?.requester == purgeUser) {
                     removeSongs.add(song)
-                    total++
                 }
             } else {
                 try {
                     if (context.guild.getMemberById(song.getUserData(TrackContext::class.java)!!.requester)?.voiceState?.channel?.idLong
                             != context.guild.selfMember.voiceState?.channel?.idLong) { //there seriously HAS to be a better way to do this what the fuck
                         removeSongs.add(song)
-                        total++
                     }
                 } catch(e: Exception) { //User kicked or banned will result in above erroring out
                     removeSongs.add(song)
-                    total++
 
                     e.printStackTrace()
                 }
@@ -59,9 +55,9 @@ class CleanupCommand : CommandExecutor() {
         if(removeSongs.size > 0) queue.removeAll(removeSongs)
 
         if(purgeUser == "left") {
-            context.send().info("Removed $total songs from users no longer in voice channel.").queue()
+            context.send().info("Removed ${removeSongs.size} songs from users no longer in voice channel.").queue()
         } else {
-            context.send().info("Removed $total songs from user ${context.message.mentionedUsers[0].name} from the queue").queue()
+            context.send().info("Removed ${removeSongs.size} songs from user ${context.message.mentionedUsers[0].name} from the queue").queue()
         }
     }
 }
