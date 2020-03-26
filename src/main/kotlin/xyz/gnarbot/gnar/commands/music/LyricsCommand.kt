9319d6756896a887ec57c1fb43d705abf19f7508
kodehawa <david.alejandro.rubio@gmail.com>
2020-03-26 15:43:31 -0300

    Limit lyrics paginator to only the user who invoked the command

--- src/main/kotlin/xyz/gnarbot/gnar/commands/music/LyricsCommand.kt
+++ src/main/kotlin/xyz/gnarbot/gnar/commands/music/LyricsCommand.kt
@@ -56,6 +56,7 @@ class LyricsCommand : CommandTemplate() {
             //val icon = songObject.getString("icon")
 
             ctx.bot.eventWaiter.paginator {
+                setUser(context.user)
                 setEmptyMessage("There should be something here 👀")
                 setItemsPerPage(1)
                 title { "Lyrics for $fullTitle" }
