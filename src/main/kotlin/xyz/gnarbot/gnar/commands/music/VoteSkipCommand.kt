391a1a80b821ae1497a9357903aaaf0d020ed88b
kodehawa <david.alejandro.rubio@gmail.com>
2020-03-31 21:43:58 -0300

    I can code

--- src/main/kotlin/xyz/gnarbot/gnar/commands/music/VoteSkipCommand.kt
+++ src/main/kotlin/xyz/gnarbot/gnar/commands/music/VoteSkipCommand.kt
@@ -56,7 +56,7 @@ class VoteSkipCommand : MusicCommandExecutor(true, true, true) {
 
         manager.lastVoteTime = System.currentTimeMillis()
         manager.isVotingToSkip = true
-        val halfPeople = (context.selfMember.voiceState!!.channel!!.members.count() / 2 + 1)
+        val halfPeople = (context.selfMember.voiceState!!.channel!!.members.filter { member -> !member.user.isBot  }.count() / 2) + 1
 
         context.send().embed("Vote Skip") {
             desc {
--- src/main/kotlin/xyz/gnarbot/gnar/commands/music/search/PlayCommand.kt
+++ src/main/kotlin/xyz/gnarbot/gnar/commands/music/search/PlayCommand.kt
@@ -141,7 +141,7 @@ class PlayCommand : CommandExecutor() {
 
             manager.lastPlayVoteTime = System.currentTimeMillis()
             manager.isVotingToPlay = true
-            val halfPeople = (context.selfMember.voiceState!!.channel!!.members.count() / 2 + 1)
+            val halfPeople = (context.selfMember.voiceState!!.channel!!.members.filter { member -> !member.user.isBot  }.count() / 2) + 1
 
             context.send().embed("Play Vote") {
                 desc {
